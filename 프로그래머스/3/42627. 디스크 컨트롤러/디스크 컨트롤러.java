3ffc7219f96bb02309112657353d330cce96e235
ktbisboy <147395130+ktbisboy@users.noreply.github.com>
2025-05-14 21:35:32 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.28 ms, Memory: 97.1 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,13 +2,13 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] jobs) {
-        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-        
-        int time = 0;
-        int totalTime = 0;
-        int idx = 0;
         int size = jobs.length;
+        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (a[1] - b[1]));
+        
+        int idx = 0;
+        int time = 0;
+        int total = 0;
         while(idx < size || !pq.isEmpty()) {
             while(idx < size && jobs[idx][0] <= time) {
                 pq.add(jobs[idx]);
@@ -18,12 +18,12 @@ class Solution {
             if(!pq.isEmpty()) {
                 int[] job = pq.poll();
                 time += job[1];
-                totalTime += (time - job[0]);
+                total += time - job[0];
             } else {
                 time = jobs[idx][0];
             }
         }
         
-        return totalTime/size;
+        return total/size;
     }
 }
