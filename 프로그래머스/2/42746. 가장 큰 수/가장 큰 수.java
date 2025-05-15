87eb5fc3391bae624bfb65df223b784f265e376f
ktbisboy <147395130+ktbisboy@users.noreply.github.com>
2025-05-15 12:12:33 +0900

    [level 2] Title: 가장 큰 수, Time: 2945.25 ms, Memory: 404 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,18 +1,18 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 class Solution {
     public String solution(int[] numbers) {
-        Integer[] nums = Arrays.stream(numbers).boxed().toArray(Integer[]::new);
-
-        Arrays.sort(nums, (a, b) -> (b + "" + a).compareTo(a + "" + b));
-
-        if (nums[0] == 0) return "0";
-
-        String str = "";
-        for (int num : nums) {
-            str += num;
+        List<Integer> list = Arrays.stream(numbers).boxed().collect(Collectors.toList());
+        list.sort((a, b) -> (b + "" + a).compareTo(a + "" + b));
+        
+        if(list.get(0) == 0) return "0";
+        
+        String answer = "";
+        for(int n : list) {
+            answer += String.valueOf(n);
         }
-
-        return str;
+        
+        return answer;
     }
 }
